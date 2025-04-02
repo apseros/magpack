0f56368a1125f21cb7708428d17b0f8b627d3ac5
apseros <apseros.andreas@gmail.com>
2025-04-02 10:02:53 +0200

    Image processing and documentation

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, basedir)
 project = 'magpack'
 copyright = '2025, Andreas Apseros'
 author = 'Andreas Apseros'
-release = '0.1.5'
+release = '0.2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
